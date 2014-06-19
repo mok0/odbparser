78f9e7984b5a36bfa755c39cbb49cb72b7332cf4
Morten Kjeldgaard <mok@bioxray.dk>
2014-06-19 06:27:40 +0200

    Cleanup of setup.py, remote old html docs

--- setup.py
+++ setup.py
@@ -2,23 +2,20 @@ import os
 from distutils.core import setup, Extension
 from distutils.sysconfig import get_python_lib
 
-incdir = os.path.join(get_python_lib(plat_specific=1), 
+incdir = os.path.join(get_python_lib(plat_specific=1),
                       "numpy/core/include/numpy")
 
-module1 = Extension('odbparser',
-                    sources = ["src/odb_io.c",
-                               "src/odb_io_f.c",
-                               "src/odbparsermodule.c",
-                              ],
-                    include_dirs=[incdir]
-                    )
+odbparser = Extension('odbparser',
+                    sources=["src/odb_io.c",
+                             "src/odb_io_f.c",
+                             "src/odbparsermodule.c",
+                             ],
+                    include_dirs=[incdir])
 
-setup (name = 'odbparser',
-       version = '2.0',
-       description = 'A Python module for reading O files',
-       author = "Morten Kjeldgaard",
-       author_email = "mok@bioxray.dk",
-       ext_modules = [module1])
-
-
+setup(name='odbparser',
+      version='2.0',
+      description='Python module for reading O files',
+      author="Morten Kjeldgaard",
+      author_email="mok@bioxray.dk",
+      ext_modules=[odbparser])
 ####
